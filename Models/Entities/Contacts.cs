15fd751bf659fdd57cfbf9486c76499ad35faa3d
Filipe castro <filipecsant@gmail.com>
2021-11-21 14:52:57 -0300

    Atualizando page ao efetuar delete

--- Models/Entities/Contacts.cs
+++ Models/Entities/Contacts.cs
@@ -13,7 +13,7 @@ namespace teste.Models.Entities
         public string Sobrenome { get; set; }
         public string Endereco { get; set; }
         public string Email { get; set; }
-        public string Aniversario { get; set; }
+        public DateTime Aniversario { get; set; }
         public virtual ICollection<Phone> Phone { get; set; }
     }
 }
